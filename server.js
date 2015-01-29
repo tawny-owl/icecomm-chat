18a1ac3b650ee2a53766401e5496fc70bd599113
Tony Rizko <tony@rizkocircle.com>
2015-01-28 18:27:07 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var path = require('path');
+var express = require('express');
+var port = process.ENV.PORT || 3000;
+
+var app = express();
+
+var staticPath = path.resolve(__dirname, '/public');
+app.use(express.static(staticPath));
+
+app.listen(port, function() {
+  console.log('listening on port:', port);
+});
