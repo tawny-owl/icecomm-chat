ad982aece635c9d2d19190921ee3f476244c5f7a
Tony Rizko <tony@rizkocircle.com>
2015-01-29 00:15:45 -0800

    (fix)add Procfile

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var path = require('path');
 var express = require('express');
 var port = process.env.PORT || 3000;
 
 var app = express();
 
-var staticPath = path.resolve(__dirname, '/public');
-app.use(express.static(staticPath));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(req, res){
+  res.sendFile('index.html');
+});
 
 app.listen(port, function() {
   console.log('listening on port:', port);
