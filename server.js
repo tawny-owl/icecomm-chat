fc675e5482f083c4945eda0a1f2fed8bf04147c6
Tony Rizko <tony@rizkocircle.com>
2015-02-24 19:27:06 -0800

    (feat)add admin routes

--- server.js
+++ server.js
@@ -9,6 +9,22 @@ app.get('/', function(req, res){
   res.sendFile('index.html');
 });
 
+var adminRouter = express.Router();
+
+adminRouter.get('/', function(req, res){
+  res.send('admin dashboard');
+});
+
+adminRouter.get('/users', function(req, res){
+  res.send('all users');
+});
+
+adminRouter.get('/posts', function(req, res){
+  res.send('all posts');
+});
+
+app.use('/admin', adminRouter);
+
 app.listen(port, function() {
   console.log('listening on port:', port);
 });
